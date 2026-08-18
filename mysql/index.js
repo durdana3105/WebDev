9485b1ac164ec3db4091d1fea1766ced37d797b2
durdana3105 <durdanasultana06@gmail.com>
2026-08-18 12:33:18 +0530

    Add MySQL Node.js project

--- mysql/index.js
+++ mysql/index.js
@@ -1,13 +1,32 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 
+//connecting mysql with my vs code
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'test',
+  database: 'delta_app',
   password:'durdana3105'
 });
 
+
+try{
+connection.query("SHOW TABLES",(err,result)=>
+{
+  if(err) throw err;
+    console.log(result);
+});
+} catch(err){
+  console.log(err)
+}
+connection.end();//to automatically close the server of mysql
+
+
+//to run mysql in our cli-terminal
+//--      {mysql -u root -p}
+
+
+//to create fake data
 let createRandomUser= ()=> {
   return {
     id: faker.string.uuid(),
@@ -16,4 +35,4 @@ let createRandomUser= ()=> {
     password: faker.internet.password(),
   };
 }
-console.log(createRandomUser());
+// console.log(createRandomUser());
